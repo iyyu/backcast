19f46890f5234396905ae55ac9478b46bd59d55e
iyyu <iyucomms@gmail.com>
2017-12-11 17:31:57 -0800

    Rendered app view

--- src/views/app.js
+++ src/views/app.js
@@ -1,14 +1,18 @@
 var AppView = Backbone.View.extend({
-
+  
   el: '#app',
 
   initialize: function() {
     this.videos = new Videos();
+    //new VideoPlayer View
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    new VideoPlayerView().render();
+    new VideoListView().render();
     return this;
   },
 
